4fdf4b9e48d378832de0cb7c6cc43d6f1fa085c2
Edik <manukyan.edik11@gmail.com>
2020-05-07 16:17:33 -0700

    feature: Project structure is created.
    View engine is set tu be ejs.
    Differents views folders are created.
    Routes are set up to be loading views.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
+
+// Project Views
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Project Routes
+app.use('/', require('./routes/index'));
+
+app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 3000;
 
